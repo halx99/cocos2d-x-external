98041d253ad96185a008d80687037b858fad7f08
halx99 <halx99@163.com>
2017-03-29 23:12:20 +0800

    update deps

--- websockets/include/win32/lws_config.h
+++ websockets/include/win32/lws_config.h
@@ -6,6 +6,8 @@
 	#endif
 #endif
 
+#define LWS_INSTALL_DATADIR "C:/Program Files (x86)/libwebsockets/share"
+
 /* Define to 1 to use wolfSSL/CyaSSL as a replacement for OpenSSL.
  * LWS_OPENSSL_SUPPORT needs to be set also for this to work. */
 /* #undef USE_WOLFSSL */
@@ -13,18 +15,27 @@
 /* Also define to 1 (in addition to USE_WOLFSSL) when using the
   (older) CyaSSL library */
 /* #undef USE_OLD_CYASSL */
+/* #undef LWS_USE_BORINGSSL */
+
+/* #undef LWS_USE_MBEDTLS */
+/* #undef LWS_USE_POLARSSL */
+/* #undef LWS_WITH_ESP8266 */
+/* #undef LWS_WITH_ESP32 */
+
+/* #undef LWS_WITH_PLUGINS */
+/* #undef LWS_WITH_NO_LOGS */
 
 /* The Libwebsocket version */
-#define LWS_LIBRARY_VERSION "1.6.0"
+#define LWS_LIBRARY_VERSION "2.2.0"
 
-#define LWS_LIBRARY_VERSION_MAJOR 1
-#define LWS_LIBRARY_VERSION_MINOR 6
+#define LWS_LIBRARY_VERSION_MAJOR 2
+#define LWS_LIBRARY_VERSION_MINOR 2
 #define LWS_LIBRARY_VERSION_PATCH 0
 /* LWS_LIBRARY_VERSION_NUMBER looks like 1005001 for e.g. version 1.5.1 */
 #define LWS_LIBRARY_VERSION_NUMBER (LWS_LIBRARY_VERSION_MAJOR*1000000)+(LWS_LIBRARY_VERSION_MINOR*1000)+LWS_LIBRARY_VERSION_PATCH
 
 /* The current git commit hash that we're building from */
-/* #undef LWS_BUILD_HASH */
+#define LWS_BUILD_HASH "halx99-pc\\halx99@HALX99-PC-v2.0.0-328-g5a354ae"
 
 /* Build with OpenSSL support */
 /* #undef LWS_OPENSSL_SUPPORT */
@@ -41,8 +52,14 @@
 /* Enable libev io loop */
 /* #undef LWS_USE_LIBEV */
 
+/* Enable libuv io loop */
+/* #undef LWS_USE_LIBUV */
+
 /* Build with support for ipv6 */
 /* #undef LWS_USE_IPV6 */
+
+/* Build with support for UNIX domain socket */
+/* #undef LWS_USE_UNIX_SOCK */
 
 /* Build with support for HTTP2 */
 /* #undef LWS_USE_HTTP2 */
@@ -54,7 +71,7 @@
 #define LWS_NO_DAEMONIZE
 
 /* Build without server support */
-#define LWS_NO_SERVER
+/* #undef LWS_NO_SERVER */
 
 /* Build without client support */
 /* #undef LWS_NO_CLIENT */
@@ -70,5 +87,44 @@
 
 /* SSL server using ECDH certificate */
 /* #undef LWS_SSL_SERVER_WITH_ECDH_CERT */
+/* #undef LWS_HAVE_SSL_CTX_set1_param */
+/* #undef LWS_HAVE_X509_VERIFY_PARAM_set1_host */
 
+/* #undef LWS_HAVE_UV_VERSION_H */
+
+/* CGI apis */
+/* #undef LWS_WITH_CGI */
+
+/* whether the Openssl is recent enough, and / or built with, ecdh */
+/* #undef LWS_HAVE_OPENSSL_ECDH_H */
+
+/* HTTP Proxy support */
+/* #undef LWS_WITH_HTTP_PROXY */
+
+/* HTTP Ranges support */
+#define LWS_WITH_RANGES
+
+/* Http access log support */
+/* #undef LWS_WITH_ACCESS_LOG */
+/* #undef LWS_WITH_SERVER_STATUS */
+
+/* #undef LWS_WITH_STATEFUL_URLDECODE */
+
+/* Maximum supported service threads */
+#define LWS_MAX_SMP 1
+
+/* Lightweight JSON Parser */
+/* #undef LWS_WITH_LEJP */
+
+/* SMTP */
+/* #undef LWS_WITH_SMTP */
+
+/* OPTEE */
+/* #undef LWS_PLAT_OPTEE */
+
+/* ZIP FOPS */
+#define LWS_WITH_ZIP_FOPS
+#define LWS_HAVE_STDINT_H
+
+/* #undef LWS_FALLBACK_GETHOSTBYNAME */
 
